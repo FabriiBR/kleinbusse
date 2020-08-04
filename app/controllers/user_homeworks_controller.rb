4d7fafdfc5234d33081842d7f2ba14d18f93648a
julianoviedo <julianoviedo1@gmail.com>
2020-08-04 12:39:42 -0300

    flashcard controller added

--- app/controllers/user_homeworks_controller.rb
+++ app/controllers/user_homeworks_controller.rb
@@ -1,2 +1,21 @@
 class UserHomeworksController < ApplicationController
+  # STATUS = %w[pendiente enviado aprobado corregir]
+
+  def create
+    @user = User.find(current_user)
+    @homework = Homework.find(params[:homework_id])
+    @uh = UserHomework.new(status: "pending")
+    @uh.user = @user
+    @uh.homework = @homework
+    if @uh.save!
+      redirect_to homework_path(@homework)
+    else
+      render user_homework_path(@homework)
+    end
+  end
+
+  def update
+    @uh = UserHomework.find(params[:id])
+    @uh.update
+  end
 end
