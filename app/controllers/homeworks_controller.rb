7e3829db05e4480b446c2cae1ca69fe4b34d3b36
Gise Ares <gise.ares@gmail.com>
2020-08-06 14:06:07 -0300

    ok a la vista de homeworks por materia y lesson

--- app/controllers/homeworks_controller.rb
+++ app/controllers/homeworks_controller.rb
@@ -2,7 +2,11 @@ class HomeworksController < ApplicationController
   before_action :set_homework, only: %i[show :update]
 
   def index
-    @homework_assignatures = Assignature.joins(lessons: :homeworks).where(homeworks: {type_off: "homework"}).distinct
+   #@homework_assignatures = Assignature.find(params[:assignature_id]).joins(lessons: :homeworks).where(homeworks: {type_off: "homework"}).distinct
+    @assignature_homeworks = Homework.joins(:lesson).where(homeworks: {type_off: "homework"}).where("lessons.assignature_id =?",params[:assignature_id] ).group_by(&:lesson)
+    @assignature_flashcards = Homework.joins(:lesson).where(homeworks: {type_off: "flashcard"}).where("lessons.assignature_id =?",params[:assignature_id] ).group_by(&:lesson)
+    @assignature = Assignature.find(params[:assignature_id])
+
   end
 
   def show
