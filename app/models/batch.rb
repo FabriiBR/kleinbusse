0c2bd69ca8b6d20ed02cfacc4033baa764d47d8f
Rodrigo Villanueva <r_villanueva18@hotmail.com>
2020-08-06 13:24:35 -0300

    Assignature-Homework

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,5 +1,5 @@
 class Batch < ApplicationRecord
-  has_many :courses
+  has_many :courses, dependent: :destroy
   has_many :students, class_name: "User", foreign_key: "batch_id"
   has_many :assignatures
 
