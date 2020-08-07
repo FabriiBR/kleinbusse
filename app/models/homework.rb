d5045f9252e6908083f7bf9d685de440ecec3797
Fabricio Butron <fa.butron.rodriguez@gmail.com>
2020-08-07 11:40:33 -0400

    arreglando homework

--- app/models/homework.rb
+++ app/models/homework.rb
@@ -2,8 +2,4 @@ class Homework < ApplicationRecord
   belongs_to :lesson
   enum type_off: [:homework, :flashcard]
   validates :content, presence: true
-  validate :flashcard_type
-  def flashcard_type
-    type_off.value == 'flashcard'
-  end
 end
