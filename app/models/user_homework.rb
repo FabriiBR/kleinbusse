1c9d556140cb5c69b74aadd872a5f2b5ce5bf7d6
julianoviedo <julianoviedo1@gmail.com>
2020-08-11 10:03:31 -0300

    user_homework missspelled

--- app/models/user_homework.rb
+++ app/models/user_homework.rb
@@ -1,5 +1,5 @@
 class UserHomework < ApplicationRecord
-  belongs_to :student, class_name: "User", inverse_of: :user_homworks
+  belongs_to :student, class_name: "User", inverse_of: :user_homeworks
   belongs_to :homework
   has_one_attached :file
- end
+end
